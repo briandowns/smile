2a01267131e49ca08e147a6528e1367df9b87ec3
Brian Downs <brian.downs@gmail.com>
2017-11-23 10:47:48 -0700

    added long running smile

--- smile.go
+++ smile.go
@@ -1,5 +1,19 @@
 package smile
 
+import "time"
+
+// Smile smiles... duh...
 func Smile() string {
+	return ":)"
+}
+
+// LongRunningSmile sleeps for a period and
+// then smiles
+func LongRunningSmile(sleep string) string {
+	dur, err := time.ParseDuration(sleep)
+	if err != nil {
+		return err.Error()
+	}
+	time.Sleep(dur)
 	return ":)"
 }
